83b7b8acfd4a7e0263343b03969ea3f53669c58e
椎名潤七 <newbingtochatgpt233@outlook.com>
2024-12-02 19:20:01 +0800

    commit

--- src/main/code/io/github/sciencekex/mapper/StudentMapper.kt
+++ src/main/code/io/github/sciencekex/mapper/StudentMapper.kt
@@ -3,7 +3,7 @@ package io.github.sciencekex.mapper
 import org.apache.ibatis.annotations.Insert
 
 interface StudentMapper {
-    @Insert("insert into db_student(name, gender, age) values('小红', '女', 17)")
+    @Insert("insert into db_student(name, gender, age) values('小刚', '男', 19)")
     fun test()
 
 }
